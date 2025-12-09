39f908bb871b6d55edb8ad90a72024ace7a2db65
JoelChavas <joel.chavas@gmail.com>
2025-12-09 13:16:24 +0100

    argparse on put_together_files

--- contrastive/utils/put_together_embeddings_files.py
+++ contrastive/utils/put_together_embeddings_files.py
@@ -2,14 +2,13 @@
 import os
 import shutil
 import logging
-import tarfile
-import pandas as pd
+import argparse
 
-path_champollion = "/neurospin/dico/data/deep_folding/current/models/Champollion_V1_after_ablation"
-embeddings_subpath = "testxx_random_embeddings/full_embeddings.csv"
-output_path = "/neurospin/dico/data/deep_folding/current/models/Champollion_V1_after_ablation/embeddings/TESTXX_embeddings"
+# path_champollion = "/neurospin/dico/data/deep_folding/current/models/Champollion_V1_after_ablation"
+# embeddings_subpath = "testxx_random_embeddings/full_embeddings.csv"
+# output_path = "/neurospin/dico/data/deep_folding/current/models/Champollion_V1_after_ablation/embeddings/TESTXX_embeddings"
 
-# %%
+
 def is_it_a_file(sub_dir):
     if os.path.isdir(sub_dir):
         return False
@@ -24,6 +23,7 @@ def is_folder_a_model(sub_dir):
     else:
         logging.debug(f"\n{sub_dir} not associated to a model. Continue")
         return False
+
 
 def get_model_paths(dir_path, result = None):
     """Recursively gets all models included in dir_path"""
@@ -40,41 +40,64 @@ def get_model_paths(dir_path, result = None):
             result.append(sub_dir)
     return result
 
-# %%
-model_paths = get_model_paths(path_champollion)
 
-# %%
-model_paths
+def put_together_embeddings_files(embeddings_subpath, output_path, path_champollion):
 
-# %% [markdown]
-# 
+    model_paths = get_model_paths(path_champollion)
 
-# %%
-if not os.path.exists(output_path):
-    os.mkdir(output_path)
+    model_paths
 
-# %%
-for model_path in model_paths:
-    file_input_name = f"{model_path}/{embeddings_subpath}"
-    region = model_path.split('Champollion_V1_after_ablation/')[1].split('/')[0]
-    model = model_path.split(region+'/')[1].replace("/", "--").replace("_", "--")
-    file_output_name = f"{output_path}/{region}_{model}_embeddings.csv"
-    try:
-        shutil.copyfile(file_input_name, file_output_name)
-    except OSError as e:
-        msg = str(e)
-        if "] " in msg:
-            msg = msg.split("] ", 1)[1]
-        print(f"The following warning can be normal if you have not generated this region in your dataset: {msg}")
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
 
-# %%
-# f = tarfile.open("/neurospin/dico/data/deep_folding/current/models/Champollion_V0/embeddings/ukb40_embeddings.tar", 'r')
-# f.extractall("/neurospin/dico/data/deep_folding/current/models/Champollion_V0/embeddings/tmp")
+    for model_path in model_paths:
+        file_input_name = f"{model_path}/{embeddings_subpath}"
+        region = model_path.split('Champollion_V1_after_ablation/')[1].split('/')[0]
+        model = model_path.split(region+'/')[1].replace("/", "--").replace("_", "--")
+        file_output_name = f"{output_path}/{region}_{model}_embeddings.csv"
+        try:
+            shutil.copyfile(file_input_name, file_output_name)
+        except OSError as e:
+            msg = str(e)
+            if "] " in msg:
+                msg = msg.split("] ", 1)[1]
+            print(f"The following warning can be normal if you have not generated this region in your dataset: {msg}")
 
-# %%
-# f
+# path_champollion = "/neurospin/dico/data/deep_folding/current/models/Champollion_V1_after_ablation"
+# embeddings_subpath = "testxx_random_embeddings/full_embeddings.csv"
+# output_path = "/neurospin/dico/data/deep_folding/current/models/Champollion_V1_after_ablation/embeddings/TESTXX_embeddings"
 
-# %%
+def main():
+    """Main function to put together the embeddings"""
+    parser = argparse.ArgumentParser(
+        description="Put together the embeddings (previously generated in folder models)"
+    )
+
+    # Required arguments
+    parser.add_argument(
+        "--embeddings_subpath",
+        type=str,
+        help="Subpath where to find ambeddings in each model folder."
+    )
+    parser.add_argument(
+        "--output_path",
+        type=str,
+        help="Output path for embeddings."
+    )
+    parser.add_argument(
+        "--path_models",
+        type=str,
+        default="/neurospin/dico/data/deep_folding/current/models/Champollion_V1_after_ablation",
+        help="Path where all models lie."
+    )
+
+    args = parser.parse_args()
+    print(args)
+
+    put_together_embeddings_files(args.embeddings_subpath, args.output_path, args.path_models)
+
+if __name__ == "__main__":
+    main()
 
 
 

cecf2016c8cc1afb25974eea25e7c3a1c3bea28f
Julien LAVAL 277509 <jl277509@is243169.intra.cea.fr>
2026-09-09 12:05:45 +0200

    refactor: cleaning unused classes in ConvNet file

--- champollion/backbones/convnet.py
+++ champollion/backbones/convnet.py
@@ -44,92 +44,12 @@ class Conv3dSame(nn.Conv3d):
         )
 
 
-class _DropoutNd(nn.Module):
-    __constants__ = ['p', 'inplace']
-    p: float
-    inplace: bool
-
-    def __init__(self, p: float = 0.5, inplace: bool = False) -> None:
-        super(_DropoutNd, self).__init__()
-        if p < 0 or p > 1:
-            raise ValueError("dropout probability has to be between 0 and 1, "
-                             "but got {}".format(p))
-        self.p = p
-        self.inplace = inplace
-
-    def extra_repr(self) -> str:
-        return 'p={}, inplace={}'.format(self.p, self.inplace)
-
-
-class Dropout3d_always(_DropoutNd):
-    r"""Randomly zero out entire channels (a channel is a 3D feature map,
-    e.g., the :math:`j`-th channel of the :math:`i`-th sample in the
-    batched input is a 3D tensor :math:`\text{input}[i, j]`).
-    Each channel will be zeroed out independently on every forward call with
-    probability :attr:`p` using samples from a Bernoulli distribution.
-
-    Alwyas applies dropout also during evaluation
-
-    As described in the paper
-    `Efficient Object Localization Using Convolutional Networks`_ ,
-    if adjacent pixels within feature maps are strongly correlated
-    (as is normally the case in early convolution layers) then i.i.d. dropout
-    will not regularize the activations and will otherwise just result
-    in an effective learning rate decrease.
-
-    In this case, :func:`nn.Dropout3d` will help promote independence between
-    feature maps and should be used instead.
-
-    Args:
-        p (float, optional): probability of an element to be zeroed.
-        inplace (bool, optional): If set to ``True``, will do this operation
-            in-place
-
-    Shape:
-        - Input: :math:`(N, C, D, H, W)` or :math:`(C, D, H, W)`.
-        - Output: :math:`(N, C, D, H, W)` or :math:`(C, D, H, W)`
-                  (same shape as input).
-
-    Examples::
-
-        >>> m = nn.Dropout3d(p=0.2)
-        >>> input = torch.randn(20, 16, 4, 32, 32)
-        >>> output = m(input)
-
-    .. _Efficient Object Localization Using Convolutional Networks:
-       https://arxiv.org/abs/1411.4280
-    """
-
-    def forward(self, input: Tensor) -> Tensor:
-        return F.dropout3d(input, self.p, True, self.inplace)
-
-
 class ConvNet(pl.LightningModule):
-    r"""3D-ConvNet model class, based on
-
-    Attributes:
-        growth_rate (int) - how many filters to add each layer (`k` in paper)
-        block_config (list of 4 ints) - how many layers in each pooling block
-        num_init_features (int) - the number of filters to learn in the first
-            convolution layer
-        bn_size (int) - multiplicative factor for number of bottle neck layers
-            (i.e. bn_size * k features in the bottleneck layer)
-        drop_rate (float) - dropout rate
-        num_classes (int) - number of classification classes
-            (if 'classifier' mode)
-        in_channels (int) - number of input channels (1 for sMRI)
-        mode (str) - specify in which mode DenseNet is trained on,
-            must be "encoder" or "classifier"
-        memory_efficient (bool) - If True, uses checkpointing. Much more memory
-            efficient, but slower. Default: *False*.
-            See `"paper" <https://arxiv.org/pdf/1707.06990.pdf>`_
-    """
 
     def __init__(self, in_channels=1, encoder_depth=3, block_depth=2,
                  num_representation_features=256, linear=True,
                  adaptive_pooling=None, filters=[16,32,64], initial_kernel_size=3,
-                 initial_stride=1, max_pool=False, drop_rate=0.1, memory_efficient=False,
-                 in_shape=None):
+                 initial_stride=1, max_pool=False, drop_rate=0.1, in_shape=None):
 
         super(ConvNet, self).__init__()
 
@@ -149,9 +69,6 @@ class ConvNet(pl.LightningModule):
 
         if adaptive_pooling is None:
             # receptive field downsampled
-            #self.z_dim_h = h//2**self.encoder_depth
-            #self.z_dim_w = w//2**self.encoder_depth
-            #self.z_dim_d = d//2**self.encoder_depth
             self.z_dim_h = ComputeOutputDim(h, self.encoder_depth)
             self.z_dim_w = ComputeOutputDim(w, self.encoder_depth)
             self.z_dim_d = ComputeOutputDim(d, self.encoder_depth)
